f6941f035b094f01d80dc5744d1686c3e88e990a
Lucas Rigon <lucas@bussolasocial.com.br>
2024-05-27 22:12:54 -0300

    first commit

---
+++ src/test/java/com/fiap/mspedidoapi/MsPedidoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.mspedidoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPedidoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
