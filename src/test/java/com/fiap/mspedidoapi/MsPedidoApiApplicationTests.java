3e28ed7f93e90f050ce3db3534bb851c8bb7ebf4
Matheus Telles <matheus.telles@smartnx.com>
2024-07-01 22:14:02 -0300

    comentando testes kafka

--- src/test/java/com/fiap/mspedidoapi/MsPedidoApiApplicationTests.java
+++ src/test/java/com/fiap/mspedidoapi/MsPedidoApiApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MsPedidoApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
